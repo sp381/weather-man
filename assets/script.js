a8d18f1dbad477a9fa32f0f822a62d5d4ad3051f
sp381 <sarah.powell381@gmail.com>
2022-03-13 14:07:44 -0700

    created html layout and JS. JS has the API key for open weather app and will pull up Chicago's weather

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,37 @@
-var APIKey = "57b955f106e5397ac471f500b09676b2";
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+var containerEl = document.querySelector("#container");
+var searchCity = document.querySelector("#search-city");
+var APIkey = "57b955f106e5397ac471f500b09676b2";
+var requestURL = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=57b955f106e5397ac471f500b09676b2";
+const citySearch = document.querySelector("#city-search");
+
+var formSearchHandler = function(event) {
+    //prevent page from refreshing
+    event.preventDefault();
+
+    var username = nameInputEl.value.trim();
+    if (username) {
+        getCityInfo(username);
+
+        containerEl.textContent = "";
+        nameInputEl.value = "";
+    } else {
+
+    }
+};
+
+    fetch(requestURL)
+        .then(response => response.json())
+        .then(result => {
+            console.log(result);
+           // if (response.ok) {
+
+           // }
+            return result.citySearch
+        });
+
+// add event listeners to form and button container
+userFormEl.addEventListener("submit", formSearchHandler);
+
+        
