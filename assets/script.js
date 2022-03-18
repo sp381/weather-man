19acf920554034be95ab03c5b7f4aa0b255cced8
sp381 <sarah.powell381@gmail.com>
2022-03-17 19:18:06 -0700

    updated fetch and the API key and info, created a function to get and display the current weather

--- assets/script.js
+++ assets/script.js
@@ -1,37 +1,55 @@
 var userFormEl = document.querySelector("#user-form");
-var nameInputEl = document.querySelector("#username");
-var containerEl = document.querySelector("#container");
-var searchCity = document.querySelector("#search-city");
+var nameInputEl = document.querySelector("#city-search");
+var cardEl = document.querySelector("#cardEl");
+var cityWeather = document.querySelector("#city-weather");
 var APIkey = "57b955f106e5397ac471f500b09676b2";
-var requestURL = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=57b955f106e5397ac471f500b09676b2";
-const citySearch = document.querySelector("#city-search");
+var cityURL = "https://api.openweathermap.org/data/2.5/weather?q=";
 
 var formSearchHandler = function(event) {
     //prevent page from refreshing
     event.preventDefault();
 
-    var username = nameInputEl.value.trim();
-    if (username) {
-        getCityInfo(username);
-
-        containerEl.textContent = "";
-        nameInputEl.value = "";
-    } else {
-
+    var input = document.getElementById("city-search");
+    var inputVal = "";
+    if (input) {
+        inputVal = input.value
     }
+    console.log(input);
+    console.log(inputVal);
+
+    var getWeather = function(city) {
+        fetch(cityURL + city + "&appid=" + APIkey +  '&units=imperial')
+            .then(function(response) {
+                return response.json();
+            })    
+            .then(function(data) {
+                console.log(data);
+            //var card = document.createElement('div')      
+                cityWeather.innerHTML = ('<div class="card" style="width: 18rem;">' + 
+               //' <img src="..." class="card-img-top" alt="...">' +
+              '  <div class="card-body">' +
+                '  <h5 class="card-title">' + data.name + '</h5>' +
+                   '<p class="card-text">'+ data.main.temp + '</p>' +
+                  //'<a href="#" class="btn btn-primary">Go somewhere</a>' +
+               ' </div>' +
+              '</div>')      
+                //TODO loop over data in a for loop and render items
+                //TODO save data that reps the city into local storage
+            });
+    };    
+    getWeather(inputVal);
+
+        //loop over city's
+        for (var i = 0; i < localStorage.length; i++) {
+         //format search name
+            var city = localStorage.getItem(i);
+
+        // create a span element to hold each city's name 
+        var city = document.createElement("span");
+        containerEl.classList = "flex-row align-center";
+
+        }
+
 };
-
-    fetch(requestURL)
-        .then(response => response.json())
-        .then(result => {
-            console.log(result);
-           // if (response.ok) {
-
-           // }
-            return result.citySearch
-        });
-
 // add event listeners to form and button container
-userFormEl.addEventListener("submit", formSearchHandler);
-
-        
+userFormEl.addEventListener("submit", formSearchHandler)
